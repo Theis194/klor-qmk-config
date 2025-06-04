3205f255a12b4b037968b5f9199ebd3a12483b60
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2025-06-04 22:32:32 +0200

    current config

---
+++ keymaps/default/klor_layers.h
@@ -0,0 +1,22 @@
+#ifndef KLOR_LAYERS_H
+#define KLOR_LAYERS_H
+
+// ┌───────────────────────────────────────────────────────────┐
+// │ d e f i n e   l a y e r s                                 │
+// └───────────────────────────────────────────────────────────┘
+
+typedef enum {
+    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
+    _QWERTY,
+    _COLEMAK,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+    _GAMING,
+    _PUBG,
+    _NOITA,
+    _MINECRAFT,
+    _CS2,
+} klor_layers;
+
+#endif
